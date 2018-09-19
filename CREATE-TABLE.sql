f6e1edd47ee85f15475408700049e5b201a584d0
Rohit <rohitguleria223@outlook.comgit config --global user.email rohitguleria223@outlook.comgit config --global user.email rohitguleria223@outlook.com>
2018-09-19 16:48:49 +0530

    Geting start with sql

---
+++ CREATE-TABLE.sql
@@ -0,0 +1,5 @@
+CREATE TABLE ROHIT
+(id SERIAL PRIMARY KEY,
+origin VARCHAR NOT NULL , 
+destination  VARCHAR  NOT NULL );
+INSERT  INTO ROHIT (id ,origin,destination) VALUES(13,'New York','Delhi');
